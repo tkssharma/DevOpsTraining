ae8f79f9b4969feb7265bcce612678fa71db537c
tkssharma <tarun.softengg@gmail.com>
2019-06-27 21:47:22 +0530

    elastic-beanstalk ci/cd example

---
+++ test/userspec.js
@@ -0,0 +1,10 @@
+# index.spec.js
+
+const expect = require('chai').expect
+const server = require('./index');
+
+describe('test', () => {
+  it('should return a string', () => {
+    expect('ci with travis').to.equal('ci with travis');
+  });
+});
