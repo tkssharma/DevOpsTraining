53012695967359324b994083676ace3a30c80832
tkssharma <tarun.softengg@gmail.com>
2019-07-21 08:54:25 +0530

    updates

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
 # this environment vairables needs to be set in .env file in applciaiton root directory
 # copy this file as .env and add the appropriate values as per environment.
-# node & mysql
 export NODE_ENV="dev"
 export PORT="3001"
 export SMTP_USER='******'
